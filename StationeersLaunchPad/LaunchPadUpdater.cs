4b2ba2a93550e59b2228020886bdb5ae81b1f111
Tom Holmes <tholmes150@gmail.com>
2025-07-18 09:17:03 -0700

    allow install to be anywhere inside bepinex plugins folder (#37)

--- StationeersLaunchPad/LaunchPadUpdater.cs
+++ StationeersLaunchPad/LaunchPadUpdater.cs
@@ -17,66 +17,133 @@ namespace StationeersLaunchPad
 
     private static List<UpdateAction> UpdateActions = new();
 
-    private static string PluginPath => Path.Combine(Paths.PluginPath, "StationeersLaunchPad");
+    private static DirectoryInfo _cachedInstallDir;
+    private static DirectoryInfo InstallDir
+    {
+      get
+      {
+        if (_cachedInstallDir == null)
+        {
+          var dir = Directory.GetParent(typeof(LaunchPadUpdater).Assembly.Location);
+          if (dir == null || !dir.Exists)
+            return null;
+
+          var pluginDir = new DirectoryInfo(Paths.PluginPath);
+          var parent = dir;
+          var nested = false;
+          // ensure install path is inside bepinex plugins
+          while (parent != null)
+          {
+            if (parent.FullName == pluginDir.FullName)
+            {
+              nested = true;
+              break;
+            }
+            parent = parent.Parent;
+          }
+          if (!nested)
+            return null;
+          _cachedInstallDir = dir;
+        }
+        return _cachedInstallDir;
+      }
+    }
     private static string TargetAssetName(Github.Release release) => $"StationeersLaunchPad-{(GameManager.IsBatchMode ? "server" : "client")}-{release.TagName}.zip";
 
     public static void RunPostUpdateCleanup()
     {
-      Logger.Global.LogDebug("Running post-update cleanup");
-      foreach (var file in Directory.EnumerateFiles(PluginPath, "*.dll.bak"))
+      try
       {
-        Logger.Global.LogDebug($"Removing update backup file {file}");
-        File.Delete(Path.Combine(PluginPath, file));
+        var installDir = InstallDir;
+        if (installDir == null)
+        {
+          Logger.Global.LogWarning("Invalid install dir. skipping post update cleanup");
+          return;
+        }
+        Logger.Global.LogDebug("Running post-update cleanup");
+        foreach (var file in installDir.EnumerateFiles("*.dll.bak"))
+        {
+          // if the matching dll doesn't exist, this probably wasn't from us?
+          if (!File.Exists(file.FullName.Substring(0, file.FullName.Length - 4)))
+            continue;
+          Logger.Global.LogDebug($"Removing update backup file {file.FullName}");
+          file.Delete();
+        }
+        LaunchPadConfig.PostUpdateCleanup.Value = false;
       }
-      LaunchPadConfig.PostUpdateCleanup.Value = false;
+      catch (Exception ex)
+      {
+        Logger.Global.LogWarning($"error occurred during post update cleanup: {ex.Message}");
+      }
     }
 
     public static async UniTask RunOneTimeBoosterInstall()
     {
-      const string boosterName = "LaunchPadBooster.dll";
-      var boosterPath = Path.Combine(PluginPath, boosterName);
-      if (File.Exists(boosterPath))
+      try
       {
-        // if file exists, this is a full install so nothing to do
-        LaunchPadConfig.OneTimeBoosterInstall.Value = false;
-        return;
-      }
-      var targetTag = $"v{LaunchPadPlugin.pluginVersion}";
-      Logger.Global.Log($"Installing LaunchPadBooster from release {targetTag}");
-      var release = await Github.FetchTagRelease(targetTag);
-      if (release == null)
-      {
-        LaunchPadConfig.AutoLoad = false;
-        Logger.Global.LogError("Installation incomplete. Please download latest version from github.");
-        return;
-      }
-
-      var assetName = TargetAssetName(release);
-      var asset = release.Assets.Find(asset => asset.Name == assetName);
-      if (asset == null)
-      {
-        LaunchPadConfig.AutoLoad = false;
-        Logger.Global.LogError($"Failed to find {assetName} in release. Installation incomplete. Please download latest version from github.");
-        return;
-      }
-
-      using (var archive = await Github.FetchZipArchive(asset))
-      {
-        var entry = archive.Entries.First(entry => entry.Name == boosterName);
-        if (entry == null)
+        var installDir = InstallDir;
+        if (installDir == null)
         {
-          Logger.Global.LogError($"Failed to find {boosterName} in {assetName}. Installation incomplete. Please download latest version from github.");
-          LaunchPadConfig.AutoLoad = false;
+          Logger.Global.LogWarning("Invalid install dir. skipping booster install");
           return;
         }
-        entry.ExtractToFile(boosterPath);
-      }
+        const string boosterName = "LaunchPadBooster.dll";
+        var boosterPath = Path.Combine(installDir.FullName, boosterName);
+        if (File.Exists(boosterPath))
+        {
+          // if file exists, this is a full install so nothing to do
+          LaunchPadConfig.OneTimeBoosterInstall.Value = false;
+          return;
+        }
+        var targetTag = $"v{LaunchPadPlugin.pluginVersion}";
+        Logger.Global.Log($"Installing LaunchPadBooster from release {targetTag}");
+        var release = await Github.FetchTagRelease(targetTag);
+        if (release == null)
+        {
+          LaunchPadConfig.AutoLoad = false;
+          Logger.Global.LogError("Installation incomplete. Please download latest version from github.");
+          return;
+        }
 
-      LaunchPadConfig.OneTimeBoosterInstall.Value = false;
+        var assetName = TargetAssetName(release);
+        var asset = release.Assets.Find(asset => asset.Name == assetName);
+        if (asset == null)
+        {
+          LaunchPadConfig.AutoLoad = false;
+          Logger.Global.LogError($"Failed to find {assetName} in release. Installation incomplete. Please download latest version from github.");
+          return;
+        }
+
+        using (var archive = await Github.FetchZipArchive(asset))
+        {
+          var entry = archive.Entries.First(entry => entry.Name == boosterName);
+          if (entry == null)
+          {
+            Logger.Global.LogError($"Failed to find {boosterName} in {assetName}. Installation incomplete. Please download latest version from github.");
+            LaunchPadConfig.AutoLoad = false;
+            return;
+          }
+          entry.ExtractToFile(boosterPath);
+        }
+
+        LaunchPadConfig.OneTimeBoosterInstall.Value = false;
+      }
+      catch (Exception ex)
+      {
+        Logger.Global.LogException(ex);
+        Logger.Global.LogError("An error occurred during LaunchPadBooster install. Some mods may not function properly");
+        LaunchPadConfig.AutoLoad = false;
+      }
     }
 
     public static async UniTask CheckVersion()
     {
+      if (InstallDir == null)
+      {
+        Logger.Global.LogWarning("Invalid install dir. Skipping update check");
+        return;
+      }
+
       var latestRelease = await Github.FetchLatestRelease();
       // If we failed to get a release for whatever reason, just bail
       if (latestRelease == null)
@@ -156,7 +223,7 @@ namespace StationeersLaunchPad
           if (!entry.Name.EndsWith(".dll"))
             continue;
 
-          var path = Path.Combine(PluginPath, $"{entry.Name}");
+          var path = Path.Combine(InstallDir.FullName, $"{entry.Name}");
           UpdateAction action = File.Exists(path) ? new ReplaceFileUpdateAction(path) : new NewFileUpdateAction(path);
           UpdateActions.Add(action);
 
